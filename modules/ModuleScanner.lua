4d598d21449fd2f661f3a689634d5f73bbb952be
wwwwwDD <105591271+wwwwwDD@users.noreply.github.com>
2025-06-18 22:08:20 +0700

    Update ModuleScanner.lua

--- modules/ModuleScanner.lua
+++ modules/ModuleScanner.lua
@@ -1,27 +1,45 @@
-local ModuleScanner = {}
-local ModuleScript = import("objects/ModuleScript")
+local ScriptScanner = {}
+local LocalScript = import("objects/LocalScript")
 
 local requiredMethods = {
-    ["getMenv"] = true,
+    ["getGc"] = true,
+    ["getSenv"] = true,
     ["getProtos"] = true,
     ["getConstants"] = true,
     ["getScriptClosure"] = true,
-    ["getLoadedModules"] = true
+    ["isXClosure"] = true
 }
 
 local function scan(query)
-    local modules = {}
+    local scripts = {}
     query = query or ""
-    
-    for _i, module in pairs(getLoadedModules()) do
-        if module.Name:lower():find(query) then
-            modules[module] = ModuleScript.new(module)
+
+    for _i, v in pairs(getGc()) do
+        if type(v) == "function" and not isXClosure(v) then
+            local env = getfenv(v)
+            local script = rawget(env, "script")
+
+            if typeof(script) == "Instance"
+                and not scripts[script]
+                and (script:IsA("LocalScript") or script:IsA("ModuleScript") or script:IsA("Script"))
+                and script.Name:lower():find(query)
+                and getScriptClosure(script)
+                and script.Parent ~= nil -- не уничтожен
+            then
+                local success = pcall(function()
+                    getsenv(script)
+                end)
+
+                if success then
+                    scripts[script] = LocalScript.new(script)
+                end
+            end
         end
     end
 
-    return modules
+    return scripts
 end
 
-ModuleScanner.Scan = scan
-ModuleScanner.RequiredMethods = requiredMethods
-return ModuleScanner
+ScriptScanner.RequiredMethods = requiredMethods
+ScriptScanner.Scan = scan
+return ScriptScanner
